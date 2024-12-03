443c50918f0d95a504c4e762d9a1b70b223e9b36
Lavi sharma <lavipandit123@gmail.com>
2024-12-03 17:36:05 +0530

    added cors for the subh

--- server.js
+++ server.js
@@ -15,7 +15,10 @@ require('./config/db');
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(cors({}));
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true,  
+}));
 
 // Set up session middleware to handle OAuth session data
 app.use(
